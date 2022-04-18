a005a6d3155f33cde5be2046bf2c84832254d6bf
Asep Kohar <asep.kahar@gmail.com>
2022-04-19 00:33:50 +0700

    ⚡️ Custom emerald color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                emerald: {
+                    700: '#00705d',
+                    800: '#006048',
+                    900: '#004d30',
+                }
+            }
+        },
     },
     plugins: [require('@tailwindcss/forms')],
 }
